9d0d82251c15a881ec5012e6d5132875af1f1c07
assuy <43821373+assuy@users.noreply.github.com>
2023-11-17 20:06:03 +0100

    Update header styling

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import Logo from "./Logo";
+import Profile from "./Profile";
+
+function Header() {
+  return (
+    <div className="w-screen shadow-md">
+      <div className="w-full p-5 flex justify-between tablet:max-w-2xl tablet:mx-auto laptop:mx-auto laptop:max-w-4xl desktop:mx-auto desktop:max-w-6xl transition-all ease-in-out place-items-center">
+        <Logo />
+        <Profile />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
